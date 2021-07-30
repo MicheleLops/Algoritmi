353065b2f301330af79a77b6df30e05e0bd76ae2
mlops <sentieroluminoso@gmail.com>
2021-07-30 09:09:51 +0200

    Primo sviluppo

---
+++ it/lops/test/Ricorsione.java
@@ -0,0 +1,23 @@
+package it.lops.test;
+
+public class Ricorsione {
+
+    private int index = 0 ;
+
+    private void rec()
+    {
+        if (index < 10 ){
+            index++;
+            System.out.println("Index : " + index);
+            rec();
+        }
+    }
+
+    public Ricorsione(int a ) {
+        rec();
+    }
+
+    public static void main(String[] args) {
+         Ricorsione seq = new Ricorsione(10);
+    }
+}
